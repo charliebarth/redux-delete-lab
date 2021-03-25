ff02bda01c691acae6addec5f47b01a50e9a40fb
github username <charliepbarth@gmail.com>
2021-03-25 15:43:32 -0400

    commit tree 1e8c1f018ccc7ffc54de30b431cc1774ca5c3909

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
-
+  
   render() {
     return(
-      <div>
-        Band Component
-      </div>
+      <li>
+        {this.props.band.text}<button onClick={() => this.props.deleteBand(this.props.band.id)}>Delete</button>
+      </li>
     );
   }
 };
